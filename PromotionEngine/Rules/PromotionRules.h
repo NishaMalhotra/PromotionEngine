1c3e1797bf89d133ea93b4fb79401314d0467625
NishaMalhotra <39948171+NishaMalhotra@users.noreply.github.com>
2022-01-28 20:46:39 +0530

    Update PromotionRules.h

--- PromotionEngine/Rules/PromotionRules.h
+++ PromotionEngine/Rules/PromotionRules.h
@@ -1,1 +1,14 @@
-
+#include<iostream>
+using namespace std;
+class PromotionRules
+{
+    public:
+        PromotionRules();
+        string ruleName { get { return ToString(); }}
+        virtual bool IsApplicable(Cart cart) = 0;
+        virtual void Execute(Cart cart) = 0;
+        static bool IsEmptyCart(Cart cart)
+        {
+            return (cart == null) || cart.Items == null || cart.Items.Count == 0;
+        }
+};
