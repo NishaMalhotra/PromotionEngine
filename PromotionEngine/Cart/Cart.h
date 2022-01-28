677ac6820303e4f341e3ef6851741c8f6fca57e6
NishaMalhotra <39948171+NishaMalhotra@users.noreply.github.com>
2022-01-28 19:59:16 +0530

    Update Cart.h

--- PromotionEngine/Cart/Cart.h
+++ PromotionEngine/Cart/Cart.h
@@ -1,1 +1,27 @@
+#include<iostream>
+#include<list>
+using namespace std;
+class cart 
+{
+    private:
+        list<cartItem> ItemList();
+    public:
+        Cart()
+        {
+            Items = new List<CartItem>();
+        }
+        double TotalPrice()
+        {
+        }
+        void AddItem(SKUitem item)
+        {
+        }
 
+        void RemoveItem(string skuItemId)
+        {
+        }
+        bool IsValidSKU(string sku)
+        {
+            
+        }
+};
