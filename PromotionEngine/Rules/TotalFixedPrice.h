2bb5889b3053f96b674a2cb2e92e6155cae41d5b
NishaMalhotra <39948171+NishaMalhotra@users.noreply.github.com>
2022-01-28 20:55:33 +0530

    Update TotalFixedPrice.h

--- PromotionEngine/Rules/TotalFixedPrice.h
+++ PromotionEngine/Rules/TotalFixedPrice.h
@@ -1,1 +1,13 @@
-
+#include<iostream>
+#include<List>
+using namespace std;
+class TotalFixedPrice:public PromotionRules
+{
+    public:
+        TotalFixedPrice();
+        TotalFixedPrice(List<string> skus, int fixedPrice);
+        List<string> SKUs();
+        void Execute(Cart cart);
+        bool IsApplicable(Cart cart);
+        string ToString();
+};
