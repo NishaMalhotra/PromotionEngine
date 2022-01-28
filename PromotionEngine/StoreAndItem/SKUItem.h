eb5194a758b804d17f5c51de2f3d39b5cf097414
NishaMalhotra <39948171+NishaMalhotra@users.noreply.github.com>
2022-01-28 21:12:54 +0530

    Update SKUItem.h

--- PromotionEngine/StoreAndItem/SKUItem.h
+++ PromotionEngine/StoreAndItem/SKUItem.h
@@ -1,1 +1,12 @@
-
+#include<iostream>
+#include<Store.h>
+#include<list>
+using namespace std;
+class SKUItem 
+{
+    public:
+        SKUitem(string id, double unitPrice);
+        string getID();  
+        double getUnitPrice();
+        void UpdateUnitPrice(double price);
+};
