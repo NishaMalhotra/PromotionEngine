263a1d6b71cdde490f15511cd6d82c8601edbb58
NishaMalhotra <39948171+NishaMalhotra@users.noreply.github.com>
2022-01-28 21:10:20 +0530

    Update Store.h

--- PromotionEngine/StoreAndItem/Store.h
+++ PromotionEngine/StoreAndItem/Store.h
@@ -1,1 +1,30 @@
-
+#include<iostream>
+#include<Store.h>
+#include<list>
+using namespace std;
+class Store:public AbstractStoreClass
+{
+  public:
+    Store();
+    Cart Cart();
+    list<PromotionRules> Promotions();
+    list<SKUitem> Items();
+    Store AddSKUitem(SKUitem item);
+    Store AddPromotions(List<PromotionRule> promotions);
+    Store AddPromotion(PromotionRule promotion);
+    Store AddPromotion(string promotion);
+    void DeletePromotion(string promotion);
+    Store AddItemToCart(string itemSKU);
+    Store EmptyCart();
+    Store Checkout();
+    List<SKUitem> GetSKUitems();
+    void UpdateSKUitemUnitPrice(string sku, float price);
+    void DeleteSKUitem(string sku);
+    List<PromotionRule> GetPromotions();
+    list<SKUitem> GetAllItems();
+    SKUitem GetItem(string sku);
+    void DeleteItemFromCart(string sku);
+    float GetCartTotal();
+    Cart GetCart();
+    bool IsValidSKU(string sku);
+};
