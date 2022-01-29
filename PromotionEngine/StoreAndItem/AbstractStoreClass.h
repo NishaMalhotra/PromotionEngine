7d8222caf7cef05e3ed8651b548de95ae7f234b2
NishaMalhotra <39948171+NishaMalhotra@users.noreply.github.com>
2022-01-29 22:47:33 +0530

    Update AbstractStoreClass.h

--- PromotionEngine/StoreAndItem/AbstractStoreClass.h
+++ PromotionEngine/StoreAndItem/AbstractStoreClass.h
@@ -1,26 +1,29 @@
+#pragma once
 #include<iostream>
 #include<Store.h>
 #include<SKUItem.h>
-SKUItem.h
+#include<Cart.h>
+#include<PromotionRules.h>
 using namespace std;
 class AbstractStoreClass
 {
   public:
-    Store AddSKUitem(SKUitem item);
-    Store AddPromotions(List<PromotionRule> promotions);
-    Store AddPromotion(PromotionRule promotion);
-    Store AddPromotion(string promotion);
+    Store AddSKUItem(SKUItem item);
+    Store AddPromotions(list<PromotionRules> promotions);
+  //  Store AddPromotions(PromotionRules promotion);
+    Store AddPromotions(string promotion);
     Store AddItemToCart(string itemSKU);
     void DeletePromotion(string promotion);
     Store EmptyCart();
     Store Checkout();
     void DeleteItemFromCart(string sku);
     float GetCartTotal();
-    List<SKUitem> GetSKUitems();
+    list<SKUItem> GetSKUitems();
     void UpdateSKUitemUnitPrice(string sku, float price);
     void DeleteSKUitem(string sku);
-    List<PromotionRule> GetPromotions();
-    List<SKUitem> GetAllItems();
-    SKUitem GetItem(string sku);
+    list<PromotionRules> GetPromotions();
+    list<SKUItem> GetAllItems();
+    SKUItem GetItem(string sku);
     Cart GetCart();
 };
+
