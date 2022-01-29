5f6c29f64c0929c510c6d33f157cb82ae872ac97
NishaMalhotra <39948171+NishaMalhotra@users.noreply.github.com>
2022-01-29 22:51:05 +0530

    Update SKUItem.cpp

--- PromotionEngine/StoreAndItem/SKUItem.cpp
+++ PromotionEngine/StoreAndItem/SKUItem.cpp
@@ -1,1 +1,24 @@
-
+#include <SKUItem.h>
+using namespace std;
+SKUitem::SKUitem(string id, double unitPrice)
+{
+    if(id == NULL)
+        throw std::invalid_argument("Invalid SKUId!!");
+    if(unitPrice <=0)
+        throw std::invalid_argument("Invalid unitPrice!!");
+    
+    this->id = id;
+    this->unitPrice = unitPrice;
+}
+string SKUitem::getID()
+{
+    return id;
+}
+double SKUitem::getUnitPrice()
+{
+    return unitPrice;
+}
+void SKUitem::UpdateUnitPrice(double price)
+{
+    unitPrice = price;
+}
