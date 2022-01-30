c821047e519f7958f5f9adb14252d3219b53754a
NishaMalhotra <39948171+NishaMalhotra@users.noreply.github.com>
2022-01-30 18:35:27 +0530

    Update Cart.cpp

--- PromotionEngine/Cart/Cart.cpp
+++ PromotionEngine/Cart/Cart.cpp
@@ -1,1 +1,47 @@
+#include<Cart.h>
+#include<SKUItem.h>
+#include<string>
+Cart::Cart()
+{
+    list<CartItem> Items;
+}
+double Cart::TotalPrice()
+{
+    return 
+    //get { return Items.Sum(i => i.FinalPrice); }
+}
+bool Cart::IsValidSKU(string sku)
+{
+    return Items.Any(i => sku.Equals(i.Item.ID));
+}
 
+
+void Cart::AddItems(SKUitem item, int numberOfItems)
+{
+    for (int i = 0; i < numberOfItems; i++)
+    {
+        CartItem c = new CartItem(item,item.UnitPrice,false)
+        Items.push_back(c);
+        //Items.Add(new CartItem { Item = item, FinalPrice = item.UnitPrice, PromotionApplied = false });
+    }
+}
+
+void Cart::AddItem(SKUitem item)
+{
+    CartItem c = new CartItem(item,item.UnitPrice,false)
+    Items.push_back(c);
+    //Items.Add(new CartItem { Item = item, FinalPrice = item.UnitPrice, PromotionApplied = false });
+}
+
+void Cart::RemoveItem(string skuItemId)
+{
+    if (!IsValidSKU(skuItemId))
+        throw std::invalid_argument("Item not found on cart!");
+
+    for(auto item:Items)
+    {
+        if(item.ID == skuItemId)
+            Items.erase(skuItemId);
+    }
+    //Items.Remove(Items.FirstOrDefault(t => skuItemId.Equals(t.Item.ID)));
+}
